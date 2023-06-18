9b08723d9f66db9b518efbf20b02d4f6465d378f
Seokhwan Won <hwani163@naver.com>
2023-06-18 12:28:25 +0900

    fix: axios 추가

--- src/Fetcher.ts
+++ src/Fetcher.ts
@@ -29,7 +29,7 @@ axiosInstance.interceptors.response.use(
     },
     (error) => {
         console.log(error.response);
-        return error;
+        return error.response;
         return Promise.reject(error);
     },
 );
